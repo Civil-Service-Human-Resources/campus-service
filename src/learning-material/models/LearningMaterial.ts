aab44ef55c91b2b83beffaffcb6ddf001bf0739a
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2022-04-01 10:43:29 +0100

    added basic swagger

--- src/learning-material/models/LearningMaterial.ts
+++ src/learning-material/models/LearningMaterial.ts
@@ -1,9 +1,24 @@
-export interface LearningMaterial {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class LearningMaterial {
+  @ApiProperty()
   source: string;
+
+  @ApiProperty()
   id: string;
+
+  @ApiProperty()
   title: string;
+
+  @ApiProperty()
   description: string;
+
+  @ApiProperty()
   shortDescription?: string;
+
+  @ApiProperty()
   outcomes: string;
+
+  @ApiProperty()
   sourceHref: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import * as appInsights from 'applicationinsights';
 import { ConfigService } from '@nestjs/config';
 import { AppInsightsLogger } from './util/logging/appi-logger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +23,15 @@ async function bootstrap() {
 
   logger.setClient(appInsights.defaultClient);
   app.useLogger(logger);
+
+  const config = new DocumentBuilder()
+    .setTitle('Campus Service')
+    .setDescription('Backend service for the Campus website')
+    .setVersion('1.0')
+    .addTag('campus')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(PORT);
 }
