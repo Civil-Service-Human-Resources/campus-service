f5783d255f57dfd1c44b446bd47da78c7748d3f0
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2022-04-19 16:19:48 +0100

    added back deleted file

---
+++ src/client/cache/cache-client.interface.ts
@@ -0,0 +1,25 @@
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { AppInsightsLogger } from '../../util/logging/appi-logger';
+
+@Injectable()
+export class CacheClient<T> {
+  private readonly logger = new AppInsightsLogger(CacheClient.name);
+  constructor(@Inject(CACHE_MANAGER) readonly store: Cache) {}
+
+  async getObject(key: string): Promise<T> {
+    try {
+      this.logger.debug(`Getting data from cache with key: ${key}`);
+      const value = await this.store.get<string>(key);
+      return JSON.parse(value) as T;
+    } catch (e) {
+      this.logger.error(
+        `Couldn't get value with cache key ${key} from cache: ${e}`,
+      );
+    }
+  }
+  async setObject(key: string, value: T) {
+    const stringedValue = await JSON.stringify(value);
+    await this.store.set(key, stringedValue);
+  }
+}
