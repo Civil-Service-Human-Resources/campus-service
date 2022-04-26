93a1577417835e1ec07aaae696d1e7cc323ab9a1
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2022-04-26 17:16:56 +0100

    removed now redundant tests

--- src/strand/validators/parse-category.pipe.spec.ts
+++ src/strand/validators/parse-category.pipe.spec.ts
@@ -25,16 +25,6 @@ describe('ParseCategoryPipe', () => {
     categoryPipe = moduleRef.get<ParseCategoryPipe>(ParseCategoryPipe);
   });
 
-  it('Should correctly transform an input category', async () => {
-    const res = await categoryPipe.transform('Category_1', null);
-    expect(res).toEqual('category_1');
-  });
-
-  it('Should correctly transform an input category with a capital', async () => {
-    const res = await categoryPipe.transform('Category 2', null);
-    expect(res).toEqual('category_2');
-  });
-
   it('Should throw a bad request exception when an invalid category is passed in', async () => {
     await expect(categoryPipe.transform('Category_3', null)).rejects.toEqual(
       new BadRequestException('invalid category supplied'),
